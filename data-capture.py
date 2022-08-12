d7df5ec427bf08751c50df0a7671af3d407ac2c8
root <maresp85@gmail.com>
2022-08-12 14:28:13 -0500

    first commit

---
+++ data-capture.py
@@ -0,0 +1,45 @@
+class DataCapture:
+    inputs = []
+
+    def build_stats(self):
+        return self
+
+    def add(self, value: int):
+        if (value > 1000):
+            print(f'Cant add {value} is greater than 1000')
+        else:
+            self.inputs.append(value)
+
+    def less(self, value: int):
+        result = 0
+        for x in self.inputs: 
+            if x < value:
+                result += 1
+        print(f'Result: {result}. Only {result} values are less than {value}.')
+
+    def between(self, value1: int, value2: int):
+        result = 0
+        for x in self.inputs: 
+            if value1 <= x <= value2:
+                result += 1
+        print(f'Result: {result}. Only {result} values are between {value1} and {value2}.')
+
+    def greater(self, value: int):
+        result = 0
+        for x in self.inputs: 
+            if x > value:
+                result += 1
+        print(f'Result: {result}. Only {result} values are greater than {value}.')
+
+
+if __name__ == '__main__':
+    capture = DataCapture()
+    capture.add(3)
+    capture.add(9)
+    capture.add(3)
+    capture.add(4)
+    capture.add(6)
+    stats = capture.build_stats()
+    stats.less(4)
+    stats.between(3, 6)
+    stats.greater(4)
